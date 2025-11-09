11e05938db5b8bd8a9ab8386eb5a5fc685760c3a
Klim <Kushnarev.Work@yandex.ru>
2025-11-09 20:04:57 +0300

    create frontend part

---
+++ resources/views/components/panel.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->merge(['class' => 'p-4 bg-white/10 rounded-xl border border-transparent hover:border-blue-700 group transition-colors duration-1000']) }}>
+    {{ $slot }}
+</div>
